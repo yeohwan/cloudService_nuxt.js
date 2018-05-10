bac9f244e99e2682058762686d73bd6bbab16ea4
yeohwanyun <cwss1503@gmail.com>
2018-05-10 11:56:29 +0900

    2018-05-10

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="section">
+    <div class = "block">
+        About page
+    </div>
+  </section>
+</template>
+
+<script>
+import Vue from 'vue';
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,16 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        yeo
-      </h1>
-      <h2 class="subtitle">
-        yeo
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <section class="section">
+    <div class = "block">
+      <a href = "about" class = "button is-primary">Test</a>
+      <p>클라우드 서비스용 포트폴리오</p>
+      <img src="~static/logo.gif" alt=""/><br>
+
     </div>
+    <br>
+    <nuxt-link to ="/about"></nuxt-link>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
-  }
-}
+import Vue from 'vue';
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
